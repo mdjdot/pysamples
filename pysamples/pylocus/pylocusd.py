c7e46f0704b8e3880e27a0bfb20e22b357b479d0
Dejian Meng <medean@live.cn>
2020-04-21 11:15:32 +0800

    add locust

--- pysamples/pylocus/pylocusd.py
+++ pysamples/pylocus/pylocusd.py
@@ -5,3 +5,21 @@ https://github.com/mdjdot/Python-100-Days/blob/master/Day91-100/96.软件测试�
 https://www.locust.io/
 https://docs.locust.io/en/latest/installation.html
 """
+
+from locust import HttpLocust, TaskSet, task
+class UserBehavior(TaskSet):
+    @task
+    def baidu_index(self):
+        self.client.get('/')
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 30000
+    max_wait = 60000
+
+"""
+locust -f pylocusd.py --host=https://www.dotcore.xyz.com
+http://localhost:8089/
+
+"""
+    
